7a92d9102d109fea7caa7c4fb0f35d5440fe4187
Jonathan Angeles <jangeles@gangsecurity.com>
2020-08-05 14:38:57 -0500

    Extended SSTI add

--- py3webfuzz/test_examples/test_sqli-1.py
+++ py3webfuzz/test_examples/test_sqli-1.py
@@ -7,12 +7,12 @@ if __name__ == "__main__":
     from py3webfuzz import fuzzdb, encoderFuncs
 
     # Instantiate a Class Object that give you access to a set of SQLi values
-    sqli_detect_payload = fuzzdb.Attack.AttackPayloads.SQLi.Detect()
+    extended_payload = fuzzdb.Extended()
     # Getting Access to those values through a list
-    for index, payload in enumerate(sqli_detect_payload.Generic_SQLI):
+    for index, payload in enumerate(extended_payload.ssti):
         print(f"Payload: {index} Value: {payload}")
-        # Using encoderFuncs you can get different handy encodings to develop exploits
 
-        print(f"SQLi Char Encode: {encoderFuncs.sqlchar_encode(payload)}")
+        # Using encoderFuncs you can get different handy encodings to develop exploits
+        print(f"SSTI Char Encode: {encoderFuncs.sqlchar_encode(payload)}")
 
 
